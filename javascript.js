6f6be18a09e60890cb2924db9d8cfc69e45c9994
Jefferson <jeffersonbrito@infojr.com.br>
2014-12-19 11:57:42 -0300

    Adiciona js do simulador de batalha

--- javascript.js
+++ javascript.js
@@ -5,3 +5,37 @@ var simulador = function (f1, a2) {
         return 'Second';
     }
 };
+
+var simulador2 = function () {
+    var chars, char1, char2, totalDamage, i = 0;
+
+    char1 = [Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 5)];
+    char1[3] = char1[2] * 5;
+
+    char2 = [Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 5)];
+    char2[3] = char2[2] * 5;
+
+    chars = [char1, char2];
+
+    while (chars[0][3] > 0 && chars[1][3] > 0) {
+        totalDamage = (char1[0] * Math.ceil(Math.random() * 6)) - (char2[0] * Math.ceil(Math.random() * 6));
+
+        if (totalDamage > 0) {
+            if (i % 2 === 0) {
+                chars[1][3] = chars[1][3] - totalDamage;
+            } else {
+                chars[0][3] = chars[0][3] - totalDamage;
+            }
+        }
+
+        i += 1;
+    }
+
+    if (chars[1][3] <= 0) {
+        chars[0][4] = 'Winner';
+    } else {
+        chars[1][4] = 'Winner';
+    }
+
+    return chars;
+};
